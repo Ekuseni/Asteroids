53f46bd8e263e1678bad09913eba734f4a1864f1
Krzysztof Szymanowski <krzysztof.szymanowski@gecko-lab.com>
2020-12-07 00:10:43 +0100

    fuck

--- Assets/Classic/Scripts/Asteroid.cs
+++ Assets/Classic/Scripts/Asteroid.cs
@@ -1,64 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Mathematics;
 using Unity.Jobs;
 using Unity.Collections;
 using Unity.Burst;
-using Unity.Mathematics;
-using UnityEngine.Jobs;
 
 public class Asteroid : MonoBehaviour
 {
-    //private SpriteRenderer spriteRenderer;
-    //private PolygonCollider2D polygonCollider;
-    //private Rigidbody2D rb2D;
 
 
-    //public SpriteRenderer SpriteRenderer { get { return spriteRenderer; } }
-    //public PolygonCollider2D PolygonCollider2D { get { return polygonCollider; } }
-    //public Rigidbody2D Rigidbody2D { get { return rb2D; } }
-
-    public static NativeArray<float3> positions = new NativeArray<float3>();
-    public static NativeArray<float3> displacements = new NativeArray<float3>();
+    public static NativeArray<float3> positions;
+    public static NativeArray<float3> displacements;
+    public static NativeArray<float3> rotations;
+    public static NativeArray<float> angularVelocities;
+    public static NativeArray<CapsulecastCommand> raycastCommands;
+    public static NativeArray<RaycastHit> raycastHits;
+    public static NativeArray<Quaternion> quaternions;
 
     public Vector3 displacement;
-
+    public float angularVelocity;
     private void Awake()
     {
-        //spriteRenderer = GetComponent<SpriteRenderer>();
-        //polygonCollider = GetComponent<PolygonCollider2D>();
-        //rb2D = GetComponent<Rigidbody2D>();
-
-        
-
         float x = UnityEngine.Random.Range(-1f, 1f);
         float y = UnityEngine.Random.Range(-1f, 1f);
 
         displacement = new Vector3(x, y, 0);
-        
-
-        //float force = Random.Range(10f, 20f);
-
-        //rb2D.velocity = /*(*/new Vector2(x,y)/* * force)*/;
+        angularVelocity = UnityEngine.Random.Range(-15f, 15f);
     }
-
-
-    //private void Update()
-    //{
-    //    transform.position += displacement * Time.deltaTime;
-    //}
-
-
 }
 
 [BurstCompile]
-public struct MoveJob : IJobParallelForTransform
-{   
-    public NativeArray<float3> displacement;
+public struct MoveJob : IJobParallelFor
+{
+    public NativeArray<float3> position;
+    public NativeArray<float3> rotation;
+    public NativeArray<CapsulecastCommand> raycasts;
+    [ReadOnly] public float3 halfExtent;
+    [ReadOnly] public float radius;
+    [ReadOnly] public NativeArray<float3> displacement;
+    [ReadOnly] public NativeArray<float> angularVelocity;
     [ReadOnly] public float deltaTime;
+    [ReadOnly] public float3 boundsMin;
+    [ReadOnly] public float3 boundsMax;
+    [ReadOnly] public float3 boundsSize;
 
-    public void Execute(int index, TransformAccess transform)
+    public void Execute(int index)
     {
-        transform.position += new Vector3(displacement[index].x, displacement[index].y, displacement[index].z)  * deltaTime;
+        float distance = ((Vector3)(displacement[index] * deltaTime)).magnitude;
+        raycasts[index] = new CapsulecastCommand(position[index] + new float3(0,0,5), position[index] - new float3(0, 0, 5), radius, displacement[index], distance);
+
+        Debug.DrawRay(position[index], displacement[index]);
+
+        position[index] += displacement[index] * deltaTime;
+
+        if (position[index].x > boundsMax.x)
+        {
+            position[index] = new float3(position[index].x - boundsSize.x, position[index].y, position[index].z);
+        }
+
+        if (position[index].y > boundsMax.y)
+        {
+            position[index] = new float3(position[index].x, position[index].y - boundsSize.y, position[index].z);
+        }
+
+        if (position[index].x < boundsMin.x)
+        {
+            position[index] = new float3(position[index].x + boundsSize.x, position[index].y, position[index].z);
+        }
+
+        if (position[index].y < boundsMin.y)
+        {
+            position[index] = new float3(position[index].x, position[index].y + boundsSize.y, position[index].z);
+        }
+
+        rotation[index] = new float3(0f, 0f, rotation[index].z + (angularVelocity[index] * deltaTime));
     }
 }
+
+//[BurstCompile]
+//public struct PrepareRaycastCommands : IJobParallelFor
+//{
+//    public float deltaTime;
+
+//    public NativeArray<RaycastCommand> raycasts;
+//    [ReadOnly]
+//    public NativeArray<float3> velocities;
+//    [ReadOnly]
+//    public NativeArray<float3> positions;
+
+
+//    public void Execute(int i)
+//    {
+//        float distance = ((Vector3)(velocities[i] * deltaTime)).magnitude;
+//        raycasts[i] = new RaycastCommand(positions[i], velocities[i], distance);
+//    }
+//}
