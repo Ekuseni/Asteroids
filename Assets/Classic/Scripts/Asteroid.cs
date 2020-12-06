fd8c3c88da4855fa53b6e36b3105295119c92a92
Krzysztof Szymanowski <krzysztof.szymanowski@gecko-lab.com>
2020-12-07 00:23:47 +0100

    an idea

--- Assets/Classic/Scripts/Asteroid.cs
+++ Assets/Classic/Scripts/Asteroid.cs
@@ -1,99 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Jobs;
-using Unity.Mathematics;
-using Unity.Jobs;
-using Unity.Collections;
-using Unity.Burst;
 
-public class Asteroid : MonoBehaviour
+public class Asteroid : PooledObject
 {
 
+    [SerializeField]
+    private float velocityMultiplier;
 
-    public static NativeArray<float3> positions;
-    public static NativeArray<float3> displacements;
-    public static NativeArray<float3> rotations;
-    public static NativeArray<float> angularVelocities;
-    public static NativeArray<CapsulecastCommand> raycastCommands;
-    public static NativeArray<RaycastHit> raycastHits;
-    public static NativeArray<Quaternion> quaternions;
-
-    public Vector3 displacement;
-    public float angularVelocity;
     private void Awake()
     {
-        float x = UnityEngine.Random.Range(-1f, 1f);
-        float y = UnityEngine.Random.Range(-1f, 1f);
-
-        displacement = new Vector3(x, y, 0);
-        angularVelocity = UnityEngine.Random.Range(-15f, 15f);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * velocityMultiplier;
+        GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-15f, 15f);
     }
 }
 
-[BurstCompile]
-public struct MoveJob : IJobParallelFor
-{
-    public NativeArray<float3> position;
-    public NativeArray<float3> rotation;
-    public NativeArray<CapsulecastCommand> raycasts;
-    [ReadOnly] public float3 halfExtent;
-    [ReadOnly] public float radius;
-    [ReadOnly] public NativeArray<float3> displacement;
-    [ReadOnly] public NativeArray<float> angularVelocity;
-    [ReadOnly] public float deltaTime;
-    [ReadOnly] public float3 boundsMin;
-    [ReadOnly] public float3 boundsMax;
-    [ReadOnly] public float3 boundsSize;
 
-    public void Execute(int index)
-    {
-        float distance = ((Vector3)(displacement[index] * deltaTime)).magnitude;
-        raycasts[index] = new CapsulecastCommand(position[index] + new float3(0,0,5), position[index] - new float3(0, 0, 5), radius, displacement[index], distance);
-
-        Debug.DrawRay(position[index], displacement[index]);
-
-        position[index] += displacement[index] * deltaTime;
-
-        if (position[index].x > boundsMax.x)
-        {
-            position[index] = new float3(position[index].x - boundsSize.x, position[index].y, position[index].z);
-        }
-
-        if (position[index].y > boundsMax.y)
-        {
-            position[index] = new float3(position[index].x, position[index].y - boundsSize.y, position[index].z);
-        }
-
-        if (position[index].x < boundsMin.x)
-        {
-            position[index] = new float3(position[index].x + boundsSize.x, position[index].y, position[index].z);
-        }
-
-        if (position[index].y < boundsMin.y)
-        {
-            position[index] = new float3(position[index].x, position[index].y + boundsSize.y, position[index].z);
-        }
-
-        rotation[index] = new float3(0f, 0f, rotation[index].z + (angularVelocity[index] * deltaTime));
-    }
-}
-
-//[BurstCompile]
-//public struct PrepareRaycastCommands : IJobParallelFor
-//{
-//    public float deltaTime;
-
-//    public NativeArray<RaycastCommand> raycasts;
-//    [ReadOnly]
-//    public NativeArray<float3> velocities;
-//    [ReadOnly]
-//    public NativeArray<float3> positions;
-
-
-//    public void Execute(int i)
-//    {
-//        float distance = ((Vector3)(velocities[i] * deltaTime)).magnitude;
-//        raycasts[i] = new RaycastCommand(positions[i], velocities[i], distance);
-//    }
-//}
--- Assets/Classic/Scripts/GameManager.cs
+++ Assets/Classic/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ using Unity.Burst;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject asteroidPrefab;
+    private Asteroid asteroidPrefab;
     [SerializeField]
     private Vector2Int gridSize;
     // Start is called before the first frame update
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private static float spawnDistance = 5f;
 
-    Bounds bounds;
+    private static Bounds bounds;
 
     private static List<Asteroid> asteroids = new List<Asteroid>();
 
@@ -45,67 +45,21 @@ public class GameManager : MonoBehaviour
         {
             for (int y = 0; y < gridSize.y; y++)
             {
-                asteroids.Add(Instantiate(asteroidPrefab).GetComponent<Asteroid>());
+                Asteroid newAsteroid = asteroidPrefab.GetPooledInstance<Asteroid>();
+
+                asteroids.Add(newAsteroid);
                 asteroids[asteroids.Count - 1].transform.position = new Vector3(bounds.min.x + x * spawnDistance + spawnDistance / 2, bounds.min.y + y * spawnDistance + spawnDistance / 2, 0);
             }
         }
 
-        
-
-        Asteroid.displacements = new NativeArray<float3>(asteroids.Count, Allocator.Persistent);
-        Asteroid.positions = new NativeArray<float3>(asteroids.Count, Allocator.Persistent);
-        Asteroid.angularVelocities = new NativeArray<float>(asteroids.Count, Allocator.Persistent);
-        Asteroid.rotations = new NativeArray<float3>(asteroids.Count, Allocator.Persistent);
-        Asteroid.raycastCommands = new NativeArray<CapsulecastCommand>(asteroids.Count, Allocator.Persistent);
-        Asteroid.raycastHits = new NativeArray<RaycastHit>(asteroids.Count, Allocator.Persistent);
-        Asteroid.quaternions = new NativeArray<Quaternion>(asteroids.Count, Allocator.Persistent);
 
     }
 
-    private void OnDisable()
-    {
-        Asteroid.displacements.Dispose();
-        Asteroid.positions.Dispose();
-    }
+    
 
     private void Update()
     {
-        for (int i = 0; i < asteroids.Count; i++)
-        {
-            Asteroid.displacements[i] = asteroids[i].displacement;
-            Asteroid.positions[i] = asteroids[i].transform.position;
-            Asteroid.rotations[i] = asteroids[i].transform.rotation.eulerAngles;
-            Asteroid.angularVelocities[i] = asteroids[i].angularVelocity;
-            Asteroid.quaternions[i] = asteroids[i].transform.rotation;
-        }
-
-        MoveJob moveJob = new MoveJob
-        {
-            deltaTime = Time.deltaTime,
-            displacement = Asteroid.displacements,
-            position = Asteroid.positions,
-            angularVelocity = Asteroid.angularVelocities,
-            rotation = Asteroid.rotations,
-            boundsMax = bounds.max,
-            boundsMin = bounds.min,
-            boundsSize = bounds.size,
-            raycasts = Asteroid.raycastCommands,
-            radius = asteroids[0].GetComponent<CapsuleCollider>().radius
-        };
-
-        JobHandle moveJobHande = moveJob.Schedule(asteroids.Count, 32);
-        JobHandle raycastJobHandle = CapsulecastCommand.ScheduleBatch(Asteroid.raycastCommands, Asteroid.raycastHits, 32, moveJobHande);
-        raycastJobHandle.Complete();
-
-        for (int i = 0; i < asteroids.Count; i++)
-        {
-            asteroids[i].transform.position = Asteroid.positions[i];
-            asteroids[i].transform.rotation = Quaternion.Euler(Asteroid.rotations[i].x, Asteroid.rotations[i].y, Asteroid.rotations[i].z);
-            if(Asteroid.raycastHits[i].normal != Vector3.zero)
-            {
-                asteroids[i].GetComponent<SpriteRenderer>().color = Color.red;
-            }
-        }
+        
     }
 }
 
